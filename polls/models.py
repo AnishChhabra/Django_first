dc3dcba55d11e6c57abfb225d4a31286e1154c79
Anish Chhabra <anish.chhabra5@gmail.com>
2016-06-29 12:08:57 +0530

    Indentation errors corrected.

--- polls/models.py
+++ polls/models.py
@@ -6,19 +6,19 @@ from django.utils import timezone
 #Each field is represented by an instance of a FIeld class
 #that tells Django abt the type of data in that field.
 class Question(models.Model):
-	question_text = models.CharField(max_length=200)
-	pub_date = models.DateTimeField('date published')
-	def __str__(self):
-		return self.question_text
-	def was_published_recently(self):
-		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 
 class Choice(models.Model):
-	question = models.ForeignKey(Question, on_delete=models.CASCADE)
-	choice_text = models.CharField(max_length=200)
-	votes=models.IntegerField(default=0)
-	def __str__(self):
-		return self.choice_text
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes=models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
 # Create your models here.
