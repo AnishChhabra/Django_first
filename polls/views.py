c5dc1393ae0bc35c32a143e86a24b18973ef985a
Anish Chhabra <anish.chhabra5@gmail.com>
2016-06-30 08:43:38 +0530

    Add files via upload

--- polls/views.py
+++ polls/views.py
@@ -10,16 +10,17 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
+        """Return the last five published questions."""
         return Question.objects.order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
-    model=Question
+    model = Question
     template_name = 'polls/detail.html'
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
-    
+
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
