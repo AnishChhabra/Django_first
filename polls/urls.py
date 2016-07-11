51fe16416357bd8f7b17d6270a001fec6f7e0fa7
Anish Chhabra <anish.chhabra5@gmail.com>
2016-07-11 21:19:49 +0530

    New views created.
    Create user, auth user, login, signup, changepw and check pw views and their html pages created. urls added.

--- polls/urls.py
+++ polls/urls.py
@@ -4,12 +4,49 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^$',
+    	views.IndexView.as_view(),
+    	name='index'
+    ),
+    url(r'^(?P<pk>[0-9]+)/$',
+    	views.DetailView.as_view(),
+    	name='detail'
+    ),
     url(
     	r'^(?P<pk>[0-9]+)/results/$',
     	views.ResultsView.as_view(),
     	name='results'
     ),
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$',
+    	views.vote,
+    	name='vote'
+    ),
+    url(r'^create_user/$',
+    	views.create_user,
+    	name='create_user'
+    ),
+    url(r'^user_auth/$',
+    	views.user_auth,
+    	name='user_auth'
+    ),
+    url(r'^login/$',
+    	views.login,
+    	name='login'
+    ),
+    url(r'^signup/$',
+    	 views.signup,
+    	 name='signup'
+    ),
+    url(r'^change_pw/$',
+    	views.change_pw,
+    	name='change_pw'
+    ),
+    url(r'^check_pw/$',
+    	views.check_pw,
+    	name='check_pw'
+    ),
+    url(r'^log_out/$',
+    	views.log_out,
+    	name='log_out'
+    ),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, get_object_or_404
-from django.template import loader
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.utils import timezone
 from django.views import generic
 
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth. decorators import login_required
+from django.contrib.auth.models import User, Permission
+
+from mysite import settings
 from .models import Question, Choice
 
 class IndexView(generic.ListView):
@@ -30,6 +34,7 @@ class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
+@login_required
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
@@ -43,4 +48,62 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+def create_user(request):
+    firstname = request.POST['Firstname']
+    lastname = request.POST['Lastname']
+    username = request.POST['Username']
+    email = request.POST['Email']
+    password = request.POST['Password']
+    # Create a new user.
+    user = User.objects.create_user(username, email, password)
+    user.firstname = firstname
+    user.lastname = lastname
+    user.save()
+    return HttpResponseRedirect(reverse('polls:index'))
+
+def user_auth(request):
+    username = request.POST['Username']
+    password = request.POST['Password']
+    user = authenticate(username=username, password=password)
+
+    if user is None:
+        return HttpResponse("Incorrect username or password.")
+    elif user.is_active:
+        login(request, user)
+        return HttpResponseRedirect(reverse('polls:index'))
+    else:
+        return HttpResponse("The account has been disabled.")
+
+def login(request):
+    return render(request, 'polls/login.html/')
+
+def signup(request):
+    return render(request, 'polls/signup.html/')
+
+@login_required
+def change_pw(request):
+    return render(request, 'polls/change_pw.html/')
+
+@login_required
+def check_pw(request):
+    old_pw = request.POST['Old_pw']
+    new_pw = request.POST['New_pw']
+    new_pw1 = request.POST['New_pw1']
+    if old_pw != password:
+        return render(request, 'polls/change_pw.html')
+    elif new_pw != new_pw1:
+        return render(request, 'polls/change_pw.html')
+    elif old_pw == new_pw:
+        return render(request, 'polls/change_pw.html')
+    else:
+        user.set_password(new_pw)
+        user.save()
+        return HttpResponseRedirect(reverse('polls:index'))
+
+def log_out(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('polls:login'))
+
+
 # Create your views here.
